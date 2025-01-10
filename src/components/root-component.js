9b5f7266da7af65e88a4cf5a55d9a5bd050078be
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-10 17:10:13 +0000

    add product component and call products api

--- src/components/root-component.js
+++ src/components/root-component.js
@@ -3,6 +3,7 @@ import {createBrowserRouter} from 'react-router-dom'
 import { MainComponent } from '../components/pages/main_component/MainComponent';
 import SignInForm  from './pages/auth/SignInForm';
 import SignUpForm from './pages/auth/SignUpForm';
+import { Product } from './pages/product/Product';
 const router=createBrowserRouter([{
     path:'/',
     element:<MainComponent />,
@@ -15,6 +16,10 @@ const router=createBrowserRouter([{
         {
             path:'signup',
             element:<SignUpForm/>
+        },
+        {
+            path:'products',
+            element:<Product/>
         }
     ]
 }])
