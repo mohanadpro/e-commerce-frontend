857c5e0c4cec29fd04191ac52b4852e2a4daed32
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-17 08:27:43 +0000

    add pagination to products

--- src/components/pages/product/Products.js
+++ src/components/pages/product/Products.js
@@ -1,33 +1,55 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Product } from './Product';
 import { Col, Row } from 'react-bootstrap';
 import { useProduct, useSetProducts } from '../../../contexts/ProductContext';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Spinner } from 'react-spinner';
 export const Products = () => {
+    const [isFirstTimeLoading, setIsFirstTimeLoading] = useState(true)
+    const[next, setNext] = useState('')
+    const [currentPage, setCurrentPage] = useState(1)
     const products = useProduct()
     const setProducts = useSetProducts()
     const getProducts = async () => {
-        await axios.get('/products')
+        await axios.get('/products?page='+currentPage)
             .then(res => {
-                setProducts(res.data.results)
+                if(isFirstTimeLoading){
+                    setProducts(res.data.results)
+                    setIsFirstTimeLoading(false)
+                }
+                else
+                    setProducts(products=>[...products, ...res.data.results])
+                setNext(res.data.next)
+                if(res.data.next!=null){
+                    console.log('next')
+                    setCurrentPage(currentPage+1)
+                }
             })
-    }
+    }    
     useEffect(() => {
         getProducts()
     }, [])
-    const handleGetProducts = ()=>{
-        console.log('Hello world')
-    }
-    return (
-        <div style={{ marginTop: '85px' }} onScroll={()=>handleGetProducts()}>
-            <Row className='mt-3'>
-                {products?.map((product, id) =>
-                <Col md={3} key={id}>
-                    <Product product={product} id={id}/>
-                </Col>
-                )}                                         
-            </Row>
-        </div>
 
+    return (
+        <div style={{ marginTop: '85px'}}>
+          <Row className='mt-3' >
+            {products.length && <InfiniteScroll
+            dataLength={products.length}
+            hasMore={ next != null ? true : false }
+            children=
+                {products.map((product, id) =>
+                <Col md={12} key={id}>
+                  {products.length ?  <Product product={product} id={id}/> :
+                  <div> No results </div>}
+                </Col>
+                )}
+            loader={<h4> Loading... </h4>}
+            next={getProducts}
+            /> 
+            }                                    
+            </Row>
+        
+        </div>
     )
 }
