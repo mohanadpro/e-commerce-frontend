64cb78f9c1ce262c59135589fb674cf967590a10
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-17 16:34:31 +0000

    update address fix bug in axios call and enhance a bit the style

--- src/components/pages/address/Address.js
+++ src/components/pages/address/Address.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useCurrentUser } from '../../../contexts/CurrentUserContext'
+import { Button, Col, Row } from 'react-bootstrap'
+import style from '../../../assets/styles/Button.module.css'
 
 export const Address = () => {
     const currentUser = useCurrentUser()
     const [ profile, setProfile ] = useState({})
-    const getUseProfile = async (e)=>{
-        e.preventDefault()
+    const [ isShippingToProfileAdd, setIsShippingToProfileAddress ] = useState(true)
+    const getUseProfile = async ()=>{
+
         try{
-        const { data } = await axios.get(`/profiles/+${currentUser.id}`)
+            console.log(currentUser);
+        const { data } = await axios.get(`profiles/${currentUser.pk}`)
         
         setProfile(data)
         }catch(err){
@@ -20,12 +24,25 @@ export const Address = () => {
         getUseProfile()
     },[])
   return (
-    <div>
-        {profile.name} <br/>
-        {profile.country} <br/>
-        {profile.state} <br/>
-        {profile.street} <br/>
-        {profile.zipcode} <br/>
-    </div>
+    <Row >
+      {isShippingToProfileAdd ? <Col md={{span:4, offset:4}}>
+            <p>
+                The order will be shipped to the below address <br/><br/>
+                 {profile.name} <br/>
+                 {profile.country} <br/>
+                 {profile.state} <br/>
+                 {profile.state} <br/>
+                 {profile.street} <br/>
+                 {profile.zipcode} <br/>
+            </p>
+            <Button onClick={e=>setIsShippingToProfileAddress(false)} className={style.Button}>
+                To another address
+            </Button> 
+       </Col>:
+       <Col md={{span:4, offset:4}}>
+
+       </Col>
+       }
+    </Row>
   )
 }
