d8a906adcf6d3564037f8981e9310055a8242d20
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-20 12:15:49 +0000

    update PaymenyMethod.js to show radio buttons as options to the payment method

--- src/components/pages/paymnets/PaymentMethod.js
+++ src/components/pages/paymnets/PaymentMethod.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StripePayment } from './StripePayment'
 import { Paypal } from './Paypal'
+import { Form } from 'react-bootstrap'
 
 export const PaymentMethod = ({amount}) => {
+  const [isPaymentUsingPayPal, setIsPaymentUsingPaypal] = useState(false)
+  const [isPaymentMethodDetermined, setIsPaymentMethodDetermined] = useState(false)
+  const handlePaypalChanges = (e)=>{
+    setIsPaymentMethodDetermined(true);
+    setIsPaymentUsingPaypal(true);
+  }
+  const handleStripeChanges = (e)=>{
+    setIsPaymentMethodDetermined(true);
+    setIsPaymentUsingPaypal(false);
+  }
   return (
+    <div style={{marginTop:'91px'}}>
+      <div className='d-flex justify-content-center' >
+     {
+     !isPaymentMethodDetermined && <Form>
+        <label>Paypal 
+          <i className="fa-brands fa-cc-paypal" style={{color: '#003087'}}></i>
+        </label>
+        <input type='radio' name="typeofpaymentmethod" value='paypal' onChange={handlePaypalChanges}/><br/>
+        <label>Visa Card <i className="fa-brands fa-cc-visa" style={{color:"	#1A1F71"}}></i> , Master Card <i className="fa-brands fa-cc-mastercard" style={{color:'#EB001B'}}></i></label>
+        <input type='radio' name="typeofpaymentmethod" value='mastercard' onChange={handleStripeChanges}/>
+      </Form>
+      }
+      </div>
     <div>
-        <StripePayment amount={amount} />
-        <Paypal amount={amount}/>;
+       { isPaymentMethodDetermined && !isPaymentUsingPayPal && <StripePayment amount={amount}/> }
+       { isPaymentMethodDetermined && isPaymentUsingPayPal && <Paypal amount={amount}/> }
+    </div>
     </div>
   )
 }
