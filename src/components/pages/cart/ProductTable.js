0e45fbfa6c043bfe2f70ae90fb882ce3155e20ff
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-22 12:17:02 +0000

    update ProductTable.js enable customer to update count of selected product from cart

--- src/components/pages/cart/ProductTable.js
+++ src/components/pages/cart/ProductTable.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Table } from 'react-bootstrap';
 import Avatar from '../avatar/Avatar';
+import { useCart, useSetCart } from '../../../contexts/CartContext';
 
 export const ProductTable = (products) => {
-
+    const Cart = useCart()
+    const setCart = useSetCart()
+    const handleChangeItemsCount = (e, item) =>{
+      setCart(Cart.map(updated_product =>
+        updated_product.product === item.product ? { ...updated_product, count: e.target.value, total_price: item.price * e.target.value } : updated_product
+      ));
+    }
   return (
     <Table striped bordered hover variant="dark" style={{marginTop:'91px'}}>
     <thead>
@@ -22,7 +29,13 @@ export const ProductTable = (products) => {
             <Avatar src={product.image} height={45}/>
             </td>
             <td>{product.name}</td>
-            <td>{product.count}</td>
+            <td>
+            <select value={product.count} onChange={e=>handleChangeItemsCount(e, product)}>
+                {Array.from(Array(25), (item,i)=>{
+                 return <option value={i+1} key={i}> {i+1}</option>
+                })}
+              </select>
+            </td>
             <td>{product.price}</td>
             <td>{product.total_price} $</td>
         </tr>
