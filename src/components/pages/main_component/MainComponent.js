ebe07fe9c6254477974c2b2ffcca12da6d48ccac
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-23 07:13:38 +0000

    test updload simple project to heroku

--- src/components/pages/main_component/MainComponent.js
+++ src/components/pages/main_component/MainComponent.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-import NavBar from '../navbar/NavBar'
-import { Footer } from '../footer/Footer'
-import { Container } from 'react-bootstrap'
+// import { Outlet, useNavigate } from 'react-router-dom'
+// import NavBar from '../navbar/NavBar'
+// import { Footer } from '../footer/Footer'
+// import { Container } from 'react-bootstrap'
 
 export const MainComponent = () => {
-  const navigate = useNavigate();
-  useEffect(()=>{
-    navigate('/products')
-  },[])
+  // const navigate = useNavigate();
+  // useEffect(()=>{
+  //   navigate('/products')
+  // },[])
   return (
     <div>
       Hello world
