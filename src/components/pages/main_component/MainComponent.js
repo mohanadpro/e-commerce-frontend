a210c80355489942ec664b7cfe264b2c81968ef9
Mohanad Dahi <mohanad.13.93@gmail.com>
2025-01-23 08:09:33 +0000

    update project

--- src/components/pages/main_component/MainComponent.js
+++ src/components/pages/main_component/MainComponent.js
@@ -1,22 +1,22 @@
 import React, { useEffect } from 'react'
-// import { Outlet, useNavigate } from 'react-router-dom'
-// import NavBar from '../navbar/NavBar'
-// import { Footer } from '../footer/Footer'
-// import { Container } from 'react-bootstrap'
+import { Outlet, useNavigate } from 'react-router-dom'
+import NavBar from '../navbar/NavBar'
+import { Footer } from '../footer/Footer'
+import { Container } from 'react-bootstrap'
 
 export const MainComponent = () => {
-  // const navigate = useNavigate();
-  // useEffect(()=>{
-  //   navigate('/products')
-  // },[])
+  const navigate = useNavigate();
+  useEffect(()=>{
+    navigate('/products')
+  },[])
   return (
     <div>
       Main Component
-        {/* <NavBar/>
+        <NavBar/>
           <Container>
             <Outlet style={{'marginTop':'91px'}}/>
           </Container>
-        <Footer/> */}
+        <Footer/>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import { CurrentUserProvider } from './contexts/CurrentUserContext';
-// import { CartProvider } from './contexts/CartContext';
+import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { CartProvider } from './contexts/CartContext';
 import { RouterProvider } from 'react-router-dom';
 import router from './components/root-component';
-// import { Toaster } from 'react-hot-toast';
-// import { ProductProvider } from './contexts/ProductContext';
-// import { AddressProvider } from './contexts/AddressContext';
-// import NavBar from './components/pages/navbar/NavBar';
-
-// import reportWebVitals from './reportWebVitals';
+import { Toaster } from 'react-hot-toast';
+import { ProductProvider } from './contexts/ProductContext';
+import { AddressProvider } from './contexts/AddressContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <h1>Hello world</h1>
-    <h1>Hello world</h1>
-    <h1>Hello world</h1>
-    <RouterProvider router={router}/>
-  </div>
-//   <CurrentUserProvider>
-//   <CartProvider>
-//     <ProductProvider>
-//       <AddressProvider>
-//         <Toaster
-//           position="top-center"
-//           reverseOrder={false}
-//         />
-//         <h1>Hello world</h1>
-//         {/* <NavBar/> */}
-//         {/* <RouterProvider router={router}/> */}
-//       </AddressProvider>
-//     </ProductProvider>
-//   </CartProvider>
-// </CurrentUserProvider>
+  <CurrentUserProvider>
+  <CartProvider>
+    <ProductProvider>
+      <AddressProvider>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+        />
+        <RouterProvider router={router}/>
+      </AddressProvider>
+    </ProductProvider>
+  </CartProvider>
+</CurrentUserProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
