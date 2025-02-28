492ff6489015a0c8a63f281fa68fc658eeec7d19
unknown <mohanad.13.93@gmail.com>
2025-02-28 16:50:16 +0100

    update SendOrder.js change sended_delivery_place properity to string instead of json value

--- src/components/pages/paymnets/SendOrder.js
+++ src/components/pages/paymnets/SendOrder.js
@@ -3,12 +3,20 @@ import { axiosRes } from "../../../api/axiosDefault";
 
 export const SendOrderToServer = async(amount, Cart, setCart, currentUser, delivery_place, setAddress, navigate)=>{
     const formData = new FormData();
+    const sended_delivery_place =  (delivery_place.name
+        + ' ' + delivery_place.country
+        + ' ' + delivery_place.zipcode
+        + ', ' + delivery_place.city
+        + ' ' + delivery_place.street 
+        + '. ' + delivery_place.street_number)
+    
     formData.append('cart', JSON.stringify(Cart));
     formData.append('total_price', amount)
     delete delivery_place.image
     delete delivery_place.created_at
     delete delivery_place.updated_at
-    formData.append('delivery_place', JSON.stringify(delivery_place))
+    formData.append('email', delivery_place.email)
+    formData.append('delivery_place', sended_delivery_place)
     formData.append('customer', currentUser.pk)
     axiosRes.post('orders/',formData).then(res=>{
         setCart([])
@@ -16,9 +24,7 @@ export const SendOrderToServer = async(amount, Cart, setCart, currentUser, deliv
         toast.success('Your order has been done successfully... ', {duration:4000})
         navigate('/products')
 
-    })
-    .catch(err=>{
-
+    }).catch(err=>{
     });
 
     }
