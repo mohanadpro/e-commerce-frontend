f21fb613af2e38d2e7cf8cec0386e2b3c0a90d53
unknown <mohanad.13.93@gmail.com>
2025-03-14 18:17:08 +0100

    update footer.js give items data-testid to be able to target them from test file

--- src/components/pages/footer/Footer.js
+++ src/components/pages/footer/Footer.js
@@ -12,23 +12,23 @@ export const Footer = () => {
      <span className='copy-right'> &copy; {today.getFullYear()}</span>
     <Nav>
       <Nav.Item>
-        <Nav.Link eventKey="1" href="https://www.youtube.com/" target='blank' aria-label='youtube'>
-          <i className='fa-brands fa-youtube youtube'></i>
+        <Nav.Link eventKey="1" href="https://www.youtube.com/" target='blank' aria-label='youtube' data-testid="youtube-link">
+          <i className='fa-brands fa-youtube youtube' data-testid="youtube"></i>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="2" href='https://x.com/' target='blank' aria-label='twitter'>
-          <i className="fa-brands fa-twitter twitter" color='#1DA1F2'></i>
+          <i className="fa-brands fa-twitter twitter" color='#1DA1F2' data-testid="twitter"></i>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="3" href='https://www.instagram.com/' target='blank' aria-label='instagram'>
-          <i className="fa-brands fa-square-instagram instagram" color='#E4405F'></i>
+          <i className="fa-brands fa-square-instagram instagram" color='#E4405F' data-testid="instagram"></i>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link eventKey="4" href='https://www.instagram.com/' target='blank' aria-label='facebook'>
-          <i className="fa-brands fa-facebook facebook" color='#1877F2'></i>
+          <i className="fa-brands fa-facebook facebook" color='#1877F2' data-testid="facebook"></i>
         </Nav.Link>
       </Nav.Item>
     </Nav>
