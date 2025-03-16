f5b46bee7974665aafd37b9bad33d2c0f841a139
unknown <mohanad.13.93@gmail.com>
2025-03-16 10:37:32 +0100

    update Footer.js add data-testid to tags

--- src/components/pages/footer/Footer.js
+++ src/components/pages/footer/Footer.js
@@ -9,25 +9,25 @@ export const Footer = () => {
     },[])
   return (
     <div className="footer d-flex align-items-center justify-content-center">
-     <span className='copy-right'> &copy; {today.getFullYear()}</span>
+     <span className='copy-right' data-testid="copyright"> &copy; {today.getFullYear()}</span>
     <Nav>
       <Nav.Item>
-        <Nav.Link eventKey="1" href="https://www.youtube.com/" target='blank' aria-label='youtube' data-testid="youtube-link">
+        <Nav.Link eventKey="1" href="https://www.youtube.com/" target='_blank' aria-label='youtube' data-testid="youtube-link">
           <i className='fa-brands fa-youtube youtube' data-testid="youtube"></i>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="2" href='https://x.com/' target='blank' aria-label='twitter'>
+        <Nav.Link eventKey="2" href='https://x.com/' target='_blank' aria-label='twitter'>
           <i className="fa-brands fa-twitter twitter" color='#1DA1F2' data-testid="twitter"></i>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="3" href='https://www.instagram.com/' target='blank' aria-label='instagram'>
+        <Nav.Link eventKey="3" href='https://www.instagram.com/' target='_blank' aria-label='instagram'>
           <i className="fa-brands fa-square-instagram instagram" color='#E4405F' data-testid="instagram"></i>
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="4" href='https://www.instagram.com/' target='blank' aria-label='facebook'>
+        <Nav.Link eventKey="4" href='https://www.instagram.com/' target='_blank' aria-label='facebook'>
           <i className="fa-brands fa-facebook facebook" color='#1877F2' data-testid="facebook"></i>
         </Nav.Link>
       </Nav.Item>
