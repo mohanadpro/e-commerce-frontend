70637148ca805f1cdcbf0c09a118bae53bf9338c
unknown <mohanad.13.93@gmail.com>
2025-03-26 13:22:51 +0100

    update CategoryList.test.jsx add test delete category case

--- src/components/pages/admin-panel/category/list/CategoryList.test.jsx
+++ src/components/pages/admin-panel/category/list/CategoryList.test.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import { it, expect, describe } from 'vitest' 
+import { it, expect, describe, vi } from 'vitest' 
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CategoryList } from './CategoryList';
 import CreateEditCategory from '../create-edit-category/CreateEditCategory';
+import Swal from 'sweetalert2';  // Import SweetAlert2
+import { toast } from 'react-hot-toast';
+
+vi.mock('react-hot-toast', () => ({
+    toast: {
+      success: vi.fn(), // Mock success toast
+      error: vi.fn(),   // Mock error toast (if needed)
+    },
+  }));
+
+// Correctly mock the default export of SweetAlert2
+vi.mock('sweetalert2', () => ({
+    default: {
+      fire: vi.fn().mockResolvedValue({
+      isConfirmed: true, // Mock response of Swal.fire() to simulate user confirmation
+    }),
+    isVisible: vi.fn().mockReturnValue(true), // Mock the isVisible method to return true
+    getTitle: vi.fn().mockReturnValue({ textContent: 'Are you sure to delete test category' }), 
+    clickConfirm: vi.fn().mockImplementation(() => { // Mock clickConfirm if needed
+        return Promise.resolve({ isConfirmed: true });
+      }),
+    clickCancel: vi.fn().mockImplementation(() => { // Mock clickCancel method
+        return Promise.resolve({ isDismissed: true }); // Simulate cancel action
+      }),
+}}))
+  
 
 
 describe('Test Category List', ()=>{
@@ -71,7 +97,8 @@ describe('Test Category List', ()=>{
             expect(edit_category_button).toBeInTheDocument()
         })        
     })
-    it('Test render delete category button',async ()=>{
+    
+    it('Test render delete category button to show confirm message',async ()=>{
         render(
         <BrowserRouter>
             <CategoryList isTesting={true}/>
@@ -80,10 +107,37 @@ describe('Test Category List', ()=>{
             </Routes>
         </BrowserRouter>
         )
-        await waitFor(()=>{
-            const delete_category_list =  screen.getAllByTestId('delete_category_button')
-            expect(delete_category_list.length).toBeGreaterThanOrEqual(1)
+        
+        const delete_category_list = await screen.findAllByTestId('delete_category_button');
+        
+        fireEvent.click(delete_category_list[3]);
 
-        })
+        expect(Swal.isVisible()).toBeTruthy();
+        
+        expect(Swal.getTitle().textContent).toEqual('Are you sure to delete test category');
+    })
+
+    it('Test click on confirm message',async ()=>{
+        render(
+        <BrowserRouter>
+            <CategoryList isTesting={true}/>
+            <Routes>
+                <Route path='category-edit-category' element={<CreateEditCategory/>}/>
+            </Routes>
+        </BrowserRouter>
+        )
+        const delete_category_list = await screen.findAllByTestId('delete_category_button');
+        
+        fireEvent.click(delete_category_list[3]);
+
+        expect(Swal.isVisible()).toBeTruthy();
+        
+        expect(Swal.getTitle().textContent).toEqual('Are you sure to delete test category');
+
+        Swal.clickConfirm();
+        
+        await waitFor(()=>{
+            expect(toast.success).toHaveBeenCalledWith('category deleted');
+        })           
     })
 })
