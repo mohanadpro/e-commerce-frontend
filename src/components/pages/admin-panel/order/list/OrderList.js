f80bfeeff438507a672ff657617f5a9108fa378c
unknown <mohanad.13.93@gmail.com>
2025-03-27 20:01:11 +0100

    update OrderList.js to test it

--- src/components/pages/admin-panel/order/list/OrderList.js
+++ src/components/pages/admin-panel/order/list/OrderList.js
@@ -1,59 +1,62 @@
-// import '../../../styles/public.css';
-
 import React, { useEffect } from 'react'
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { axiosReq } from '../../../../../api/axiosDefault';
+import { axiosReq, axiosRes } from '../../../../../api/axiosDefault';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import toast from 'react-hot-toast';
+import { toast } from 'react-hot-toast';
 
-export function OrderList(props) {
+export function OrderList({isTesting}) {
       const [orders, setOrders] = useState([]);
       const [updatedOrder, setUpdatedOrder] = useState({})
       const [next, setNext] = useState('')
       const [currentPage, setCurrentPage] = useState(1)
       const [isFirstTimeLoading, setIsFirstTimeLoading] = useState(true)
-      const getOrders = async ()=>{
-        axiosReq.get('orders/allOrders/?page=' + currentPage).then(res=>{
-            if (isFirstTimeLoading) {
-                setOrders(res.data.results)
-                setIsFirstTimeLoading(false)
-            }
-            else
-            {
-                setOrders(orders => [...orders, ...res.data.results])
-            }
-            setNext(res.data.next)
-            if (res.data.next != null) {
-                setCurrentPage(currentPage + 1)
-            }
-        }).catch(err=>{
+      const getOrders = async ()=>{        
+        if(isTesting)
+            var { data } = await axiosRes.get('orders/allOrders/?page=' + currentPage, { headers: {'Authorization': `Bearer ${process.env.REACT_APP_ADMIN_TOKEN}`}});
+        else
+            var { data } = await axiosRes.get('orders/allOrders/?page=' + currentPage);
 
-        })
+        if (isFirstTimeLoading) {
+            setOrders(data.results)
+            setIsFirstTimeLoading(false)
+        }
+        else
+        {
+            setOrders(orders => [...orders, ...data.results])
+        }
+        setNext(data.next)
+        if (data.next != null) {
+            setCurrentPage(currentPage + 1)
+        }
       }
 
-      const shipOrder= (order)=>{
-        setUpdatedOrder({'created_at': order.created_at,
-            'total_price': order.total_price,
-            'customer': order.customer,
-            'delivery_place': order.delivery_place,
-            'is_delivered': true})
-        axiosReq.put('/orders/orderDetails/'+order.id+'/', updatedOrder)
-        .then(res=>{
-            toast.success('Order has been shipped successfully... ')
-        })
-        .catch(err=>{
+      const shipOrder= async (order)=>{
+        try{
+            setUpdatedOrder({'created_at': order.created_at,
+                'total_price': order.total_price,
+                'customer': order.customer,
+                'delivery_place': order.delivery_place,
+                'is_delivered': true})
+            let res;
+            if(isTesting)
+               res = axiosReq.put('/orders/orderDetails/'+order.id+'/', updatedOrder, { headers: {'Authorization': `Bearer ${process.env.REACT_APP_ADMIN_TOKEN}`}})
+            else
+               res = axiosReq.put('/orders/orderDetails/'+order.id+'/', updatedOrder)
+            console.log(res.status)
+            if(res.status == 201)
+                toast.success('Order has been shipped successfully...')
+        }catch(err){
             toast.error('There is a problem with the editing... ')
-        })
         }
+    }
+
       useEffect(() => {
         getOrders()
       }, [])
       return (
-
             <div className="mybody" style={{minHeight:'75vh'}}>
                 <div className="title">
-                    <h1>List Orders</h1>
+                    <h1 data-testid="List_Products_Text">List Orders</h1>
                 </div>
                 <div  className='d-flex flex-column justify-content-between' >
                 <InfiniteScroll
@@ -77,7 +80,7 @@ export function OrderList(props) {
 
                         {orders.map((item,i) =>
                             <tr key={i}>
-                                <td><button className='btn btn-none' onClick={()=>{shipOrder(item)}}><i className="fa-solid fa-truck"></i></button> </td>
+                                <td><button data-testid="ship_order_button" className='btn btn-none' onClick={()=>{shipOrder(item)}}><i className="fa-solid fa-truck"></i></button> </td>
                                 <td>{item.created_at}</td>
                                 <td>{item.total_price}</td>
                                 <td>{item.customer_name}</td>
@@ -92,4 +95,3 @@ export function OrderList(props) {
             </div>
       )
 }
-
