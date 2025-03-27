2f30dfc0a716743dd80d2b1e818fd91383ad950e
unknown <mohanad.13.93@gmail.com>
2025-03-27 18:25:28 +0100

    update PaymentMethod.js add data-testid to root tag

--- src/components/pages/paymnets/PaymentMethod.js
+++ src/components/pages/paymnets/PaymentMethod.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { StripePayment } from './StripePayment'
 import { Paypal } from './Paypal'
 import { Col } from 'react-bootstrap'
 import './payment-method.css'
 export const PaymentMethod = ({amount}) => {
   return (
-    <Col md={{md:5}} style={{marginTop:'90px'}} data-testid="payment-methods">
-        <StripePayment amount={amount}/> 
-        <Paypal amount={amount}/> 
-    </Col>
+      <Col md={{md:5}} style={{marginTop:'90px'}} data-testid="payment_methods">
+          <StripePayment amount={amount}/> 
+          <Paypal amount={amount}/> 
+      </Col>
   )
 }
