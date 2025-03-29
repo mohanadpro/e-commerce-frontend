388902b6022cfef260570374e69195db92414bf5
unknown <mohanad.13.93@gmail.com>
2025-03-29 23:16:31 +0100

    update axiosDefault.js change post content-type to application/json

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
